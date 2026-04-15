cafc8d55afdddb0b9fbeaa013d220dc31173485a
dineshreddy2107 <dineshseelam@gmail.com>
2026-04-15 21:48:02 +1000

    terraform

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+    common_tags = {
+        Project = var.project
+        environment = var.environment
+        terraform = true
+    }
+    az_names = slice(data.aws_availability_zones.available.names,0,2)
+}
